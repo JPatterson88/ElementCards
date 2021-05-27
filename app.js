7a7b98d22bff19cd05583ff101c3fcc1b6df3443
JPatterson88 <traverisp@gmail.com>
2021-05-27 15:19:42 -0400

    commit on master

---
+++ app.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', ()=>{
+        removePanelClass()
+        panel.classList.add('active')
+    })
+})
+
+
+function removePanelClass() {
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
+
